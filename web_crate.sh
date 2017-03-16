98aa1b65b3c4cbda0e20a0858ddf7abb6afc71c3
Amenitskiy Maxim <m.amenitskiy@gmail.com>
2017-03-16 13:39:28 +0300

    second final_configuration web_server

--- web_crate.sh
+++ web_crate.sh
@@ -7,5 +7,5 @@ mkdir ~/web/public/css
 mkdir ~/web/public/js
 mkdir ~/web/uploads
 mkdir ~/web/etc
-echo 'server {listen 80 default_server;listen [::]:80 default_server;index index.html index.htm index.nginx-debian.html;server_name _;location ^~/uploads/{root /home/box/web;}location ~*\.jpg{root /home/box/web/public/;}location /{return 404;}}' >~/web/etc/nginx.cofig
+echo 'server {listen 80 default_server;listen [::]:80 default_server;	index index.html index.htm index.nginx-debian.html;server_name _;location ^~/uploads/{root /home/box/web;}location ~*^.+\.\w+$ {root /home/box/web/public;}location /{return 404;}}' >~/web/etc/nginx.cofig
 
